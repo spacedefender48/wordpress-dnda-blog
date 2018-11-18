3be0de94ba992c04ce65399ddf4e432cbe371cd9
Plamen Kolarov <spacedefender48@gmail.com>
2018-11-18 11:11:52 +0200

    Home content

--- header.php
+++ header.php
@@ -18,7 +18,11 @@
         <div class="container-fluid wrap">
             <div class="row">
                 <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
-
+                    <div class="canvas-btn">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </div>
 
                 <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
--- index.php
+++ index.php
@@ -8,16 +8,30 @@
 
                     if ($query -> have_posts()) :
                         while($query -> have_posts()) : $query -> the_post(); ?>
-
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 main-post">
                             <div class="post-wrap">
-                                <h2 class="post-wrap--title"><?php the_title(); ?></h2>
+                                <?php 
+                                    if( has_post_thumbnail() ) {
+                                        the_post_thumbnail('original');
+                                    }
+                                ?>
+
+                                <h2 class="post-wrap--title">
+                                    <a href="<?php the_permalink();?>"><?php the_title(); ?></a>
+                                </h2>
+
                                 <div class="post-wrap--excerpt">
                                     <?php the_excerpt(); ?>
                                 </div>
+
                                 <p class="post-wrap--meta">
                                     <?php the_date();?> <span>/</span> <?php $categories = get_the_category(); echo $categories[0]->name ?> <span>/</span> BY <?php the_author(); ?>
                                 </p>
+
+                                <div class="post-wrap--read-wrap">
+                                    <a href="<?php the_permalink(); ?>" class="post-wrap--read-link">Read article</a>
+                                </div>
                             </div>
                         </div>
                 <?php
@@ -32,6 +46,89 @@
         </div> <!-- Close div.container-fluid -->
 
     </header>
+
+    <section id="content">
+        <div class="posts-wrap">
+            <div class="container-fluid">
+                    <?php 
+                    $query = new WP_query(array(
+                        'posts_per_page' => '5',
+                        'orderby' => 'post_date'
+                    ));
+
+                    $num_post = 1;
+                    $isRight = false;
+
+                    if ($query -> have_posts()) :
+                        while($query -> have_posts()) : $query -> the_post(); 
+                        
+                            if ($num_post == 1) {
+                                
+                                $num_post = 2;
+                                continue;
+                            }
+
+                            else {
+                            
+                            ?>
+
+                            <div class="row <?php if ($isRight) { echo 'right'; }?>">
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 post-image">
+                                    <a href="<?php the_permalink();?>" class="post-image--link">
+                                        <?php 
+                                            if( has_post_thumbnail() ) {
+                                                the_post_thumbnail('large');
+                                            }
+                                        ?>
+                                    </a>
+                                </div>
+
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 post-details-wrap">
+                                    <h2 class="post-details-wrap--title">
+                                        <a href="<?php the_permalink();?>"><?php the_title(); ?></a>
+                                    </h2>
+
+                                    <div class="post-details-wrap--excerpt">
+                                        <?php the_excerpt(); ?>
+                                    </div>
+
+                                    <p class="post-details-wrap--meta">
+                                        <?php the_date();?> <span>/</span> <?php $categories = get_the_category(); echo $categories[0]->name ?> <span>/</span> BY <?php the_author(); ?>
+                                    </p>
+
+                                    <div class="post-details-wrap--read-wrap">
+                                        <a href="<?php the_permalink(); ?>" class="post-details-wrap--read-link">Read article</a>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                    <?php 
+                            }
+                            
+                            // Change orientation
+                            $isRight = !$isRight;
+                        endwhile;
+                    endif;
+
+                        wp_reset_postdata();
+                    ?>
+                <div class="row load-more-posts--wrap">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="load-more-posts--link">
+                            Read more stories
+                        </div>
+
+                        <div class="overlay-background">
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+    </section>
 
 
 <?php get_footer(); ?>
